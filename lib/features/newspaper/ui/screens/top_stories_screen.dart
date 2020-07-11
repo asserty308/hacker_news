7d8167eba370659362440aa20912e58a1f776a0e
asserty308 <yannickassert@gmail.com>
2020-07-12 00:55:01 +0200

    url launch

--- lib/features/newspaper/ui/screens/top_stories_screen.dart
+++ lib/features/newspaper/ui/screens/top_stories_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_core/ui/widgets/center_progress_indicator.dart';
 import 'package:flutter_cubit/flutter_cubit.dart';
 import 'package:hacker_news/features/newspaper/data/models/item_model.dart';
 import 'package:hacker_news/features/newspaper/data/repositories/hackernews_repository.dart';
 import 'package:hacker_news/features/newspaper/state/top_stories/top_stories_cubit.dart';
-import 'package:page_turn/page_turn.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class TopStoriesScreen extends StatelessWidget {
   @override
@@ -27,26 +28,29 @@ class TopStoriesScreen extends StatelessWidget {
   Widget get _body => CubitBuilder<TopStoriesCubit, TopStoriesState>(
     builder: (context, state) {
       if (state is TopStoriesLoaded) {
-        return _pageTurn(state.stories);
+        return _listView(state.stories);
       }
 
-      return Container(color: Colors.white,);
+      return CenterProgressIndicator();
     },
-  );
-
-  Widget _pageTurn(List<ItemModel> stories) => PageTurn(
-    children: [
-      _listView(stories.getRange(0, 10).toList()),
-      _listView(stories.getRange(10, 20).toList()),
-      _listView(stories.getRange(20, 30).toList()),
-    ]
   );
 
   Widget _listView(List<ItemModel> stories) => ListView.builder(
     itemCount: stories.length,
     itemBuilder: (context, index) {
       final story = stories[index]; 
-      return ListTile(title: Text(story.title ?? 'Unknown title', style: TextStyle(color: Colors.black),),);
+      return ListTile(
+        title: Text(story.title ?? 'Unknown title', style: TextStyle(color: Colors.white),),
+        onTap: () => _showStory(story.url),
+      );
     }
   );
+
+  // Functions
+
+  void _showStory(String url) async {
+    if (await canLaunch(url)) {
+      launch(url);
+    }
+  }
 }
