122466e894f6fc23c777443bb107b842f9e7598e
asserty308 <yannickassert@gmail.com>
2023-07-24 11:23:13 +0200

    added version and licenses

--- lib/ui/pages/settings_page.dart
+++ lib/ui/pages/settings_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hacker_news/router/router.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
@@ -13,6 +14,33 @@ class SettingsPage extends StatelessWidget {
         IconButton(icon: const Icon(Icons.favorite), onPressed: () => appRouter.go('/favorites')),
       ],
     ),
-    body: const Placeholder(),
+    body: ListView(
+      children: [
+        _licensesTile(context),
+        _versionTileBuilder,
+      ],
+    ),
+  );
+
+  Widget _licensesTile(BuildContext context) => ListTile(
+    title: const Text('Open Source Licenses'),
+    onTap: () => showLicensePage(
+      context: context,
+    ),
+  );
+
+  Widget get _versionTileBuilder => FutureBuilder(
+    future: PackageInfo.fromPlatform(),
+    builder: (context, snapshot) {
+      final data = snapshot.data;
+
+      if (data == null) {
+        return const SizedBox();
+      }
+
+      return ListTile(
+        subtitle: Text('Version ${data.version}'),
+      );
+    },
   );
 }
