6ab214fdf4e774fc69124e51020b7d0c63cab6e6
asserty308 <yannickassert@gmail.com>
2023-08-12 04:55:33 +0200

    cleanup

--- lib/ui/widgets/story_listtile.dart
+++ lib/ui/widgets/story_listtile.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hacker_news/bloc/like_button/like_button_cubit.dart';
