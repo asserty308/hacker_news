233018e611833ca145170bac6da7a5f554b68887
asserty308 <yannickassert@gmail.com>
2024-01-28 17:21:14 +0100

    cleanup

--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,5 +1,3 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hacker_news/bloc/top_stories/top_stories_cubit.dart';
