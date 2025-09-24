a17df9518afcd9bc42b141c3fd3a360a9debe71b
asserty308 <yannickassert@gmail.com>
2025-09-24 15:06:01 +0200

    refactor: remove unused Appwrite configuration constants

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -1,9 +1,3 @@
 const kGitHubRepoUrl = 'https://github.com/asserty308/hacker_news';
 
-const kAppwriteProjectID = String.fromEnvironment('APPWRITE_PROJECT_ID');
-const kAppwriteDatabaseID = String.fromEnvironment('APPWRITE_DB_ID');
-const kAppwriteFavoritesCollectionID = String.fromEnvironment(
-  'APPWRITE_FAVORITES_COLLECTION_ID',
-);
-
 const kContentPadding = 16.0;
