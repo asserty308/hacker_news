3ca36850c7e2aa57f605cb869ee3cadca32543ed
asserty308 <yannickassert@gmail.com>
2025-10-12 13:57:44 +0200

    chore: update android project

--- android/app/src/main/kotlin/com/example/hacker_news/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.hacker_news
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/de/asserty/hacker_news/MainActivity.kt
@@ -0,0 +1,5 @@
+package de.asserty.hacker_news
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
